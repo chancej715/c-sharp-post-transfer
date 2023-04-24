1568c49fa4e9fec9feb6741b64d89399c68f6e47
Chance Johnson <chancej715@gmail.com>
2023-04-24 15:14:06 -0500

    'application/octet-stream' header.

--- sharp-post-transfer.cs
+++ sharp-post-transfer.cs
@@ -1,34 +1,41 @@
-// // File
-// string file = @"data";
-
-// // Read contents of a file into a byte array
-// byte[] bytes = File.ReadAllBytes(file);
-
-// // Print binary array value of the file contents
-// foreach(byte s in bytes)
-// {
-//     Console.WriteLine(s);
-// }
-
-// https://makolyte.com/csharp-how-to-send-a-file-with-httpclient/
-
 using System.Net.Http.Headers;
 
-var filePath = @"data";
+var filePath = @"data";					// File path
+// var fileName = "data";					// File name
+var uri = "http://192.168.10.3:5000";	// Receiving server URL
 
-using (var multipartFormContent = new MultipartFormDataContent())
+// using (var multipartFormContent = new MultipartFormDataContent())
+// {
+// 	//Load file
+// 	var fileStreamContent = new StreamContent(File.OpenRead(filePath));
+	
+// 	// 
+// 	fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+// 	multipartFormContent.Add(fileStreamContent, name: fileName, fileName: fileName);
+
+// 	//Send file
+//     var httpClient = new HttpClient();
+
+// 	var response = await httpClient.PostAsync(url, multipartFormContent);
+// 	response.EnsureSuccessStatusCode();
+// 	var responseContent = await response.Content.ReadAsStringAsync();
+// }
+
+var byteData = File.ReadAllBytes(filePath);
+
+using (var content = new ByteArrayContent(byteData))
 {
-	//Load the file and set the file's Content-Type header
-	var fileStreamContent = new StreamContent(File.OpenRead(filePath));
-	fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+	content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
-	//Add the file
-	multipartFormContent.Add(fileStreamContent, name: "data", fileName: "data");
+	var httpClient = new HttpClient();
+	var response = await httpClient.PostAsync(uri, content);
 
-    var httpClient = new HttpClient();
-
-	//Send it
-	var response = await httpClient.PostAsync("http://192.168.10.3:5000", multipartFormContent);
-	response.EnsureSuccessStatusCode();
 	var responseContent = await response.Content.ReadAsStringAsync();
 }
+
+// TODO: Use "application/octet-stream" MIME type
+		// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#applicationoctet-stream
+	
+	// Currently, it's using the "MultipartFormDataContent" class to encode using "multipart/form-data" MIME type
+	// This MIME type is used when sending the values of completed HTML forms from browser to server
+		// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#multipartform-data
