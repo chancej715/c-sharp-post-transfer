5af276c73d2342367e822f67256c5a7f79b92f76
Chance Johnson <chancej715@gmail.com>
2023-04-24 18:18:03 -0500

    Get file name from path.

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+
 app = Flask(__name__)
+
 @app.route('/', methods=['POST'])
 def result():
-    f = request.files['data']
-    f.save(f.filename)
-    return 'Received !' # response to your request.
-
-   
+    for file in request.files:      
+        file = request.files[file]
+        file.save(file.filename)
+    
+    return "Success."
--- sharp-post-transfer.cs
+++ sharp-post-transfer.cs
@@ -1,34 +1,26 @@
-// // File
-// string file = @"data";
-
-// // Read contents of a file into a byte array
-// byte[] bytes = File.ReadAllBytes(file);
-
-// // Print binary array value of the file contents
-// foreach(byte s in bytes)
-// {
-//     Console.WriteLine(s);
-// }
-
-// https://makolyte.com/csharp-how-to-send-a-file-with-httpclient/
-
 using System.Net.Http.Headers;
 
-var filePath = @"data";
+var filePath 	= @"PATH";						// File to transfer
+var url 		= "URL";						// Receiving URL
+
+var fileName 	= Path.GetFileName(filePath);
 
 using (var multipartFormContent = new MultipartFormDataContent())
 {
-	//Load the file and set the file's Content-Type header
+	// Read file
 	var fileStreamContent = new StreamContent(File.OpenRead(filePath));
-	fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
-	//Add the file
-	multipartFormContent.Add(fileStreamContent, name: "data", fileName: "data");
+	// Add file
+	multipartFormContent.Add(fileStreamContent, name: fileName, fileName: fileName);
 
-    var httpClient = new HttpClient();
-
-	//Send it
-	var response = await httpClient.PostAsync("http://192.168.10.3:5000", multipartFormContent);
-	response.EnsureSuccessStatusCode();
-	var responseContent = await response.Content.ReadAsStringAsync();
+	try
+	{
+		// Send POST request
+    	var httpClient = new HttpClient();
+		var response = await httpClient.PostAsync(url, multipartFormContent);
+	}
+	catch (Exception e)
+	{
+		Console.WriteLine(e);
+	}
 }
